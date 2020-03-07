4bc1b1a4768ad166831857a177887275a136a53f
Joakim Gunst <joakim.gunst@nitor.com>
2020-03-07 16:55:37 +0200

    Display list of posts

--- data/contentful.ts
+++ data/contentful.ts
@@ -1,6 +1,12 @@
-import { createClient } from 'contentful'
+import { createClient } from 'contentful';
 
 export const contentfulClient = createClient({
   space: process.env.CONTENTFUL_SPACE_ID!,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
-})
+});
+
+export interface Post {
+  title: string;
+  slug: string;
+  content?: string;
+}
