32eabb6c09a2edfbd7d61c827d1553501b7404c8
Joakim Gunst <joakim.gunst@nitor.com>
2020-03-14 13:20:54 +0200

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,13 @@ import 'dayjs/locale/sv';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
+import Router from 'next/router';
+import { pageview } from '../lib/gtag';
 
 dayjs.extend(LocalizedFormat);
 dayjs.locale('sv');
+
+Router.events.on('routeChangeComplete', url => pageview(url));
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -32,6 +33,22 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Alegreya+Sans:400,700,400italic,700italic"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
           />
         </Head>
         <body>
