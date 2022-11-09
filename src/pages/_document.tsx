7a3092d116544f29420862b5c6582a598f68d747
Joakim Gunst <joakim.gunst@nitor.com>
2022-11-09 09:54:05 +0200

    Fix SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,33 @@ import Document, {
 } from 'next/document';
 import { GA_TRACKING_ID } from '../lib/gtag';
 import { siteDescription } from '../config';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
