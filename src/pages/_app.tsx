1d643923499a64b482ce16292bbf3b593627283a
Joakim Gunst <joakim.gunst@nitor.com>
2023-06-21 08:48:19 +0300

    Remove dayjs dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,10 @@ import { AppProps } from 'next/app';
 import 'normalize.css';
 import '../styles/global.css';
 import '../styles/fonts.css';
-import dayjs from 'dayjs';
-import 'dayjs/locale/sv';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import PreviewIndicator from '../components/PreviewIndicator';
 import { Analytics } from '@vercel/analytics/react';
-
-dayjs.extend(LocalizedFormat);
-dayjs.locale('sv');
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
