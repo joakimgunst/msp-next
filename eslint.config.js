43387069d474d1a00830255c43046a408ce78399
Joakim Gunst <joakim.gunst@nitor.com>
2024-12-12 06:41:33 +0200

    Add playwright

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import prettier from 'eslint-plugin-prettier/recommended';
+import playwright from 'eslint-plugin-playwright';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -14,6 +15,15 @@ const config = [
     extends: ['next/core-web-vitals', 'next/typescript'],
   }),
   prettier,
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/no-conditional-in-test': 'off',
+      'playwright/expect-expect': ['error', { assertFunctionNames: ['expectMainHeading'] }],
+    },
+  },
 ];
 
 export default config;
