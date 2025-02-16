eefad9bf7c4e0024d08fb23a5ec7dd7d774b8ac3
Joakim Gunst <joakim.gunst@nitor.com>
2025-02-16 13:09:31 +0200

    Add organization JSON-LD metadata

---
+++ src/components/ContactJsonLd.tsx
@@ -0,0 +1,21 @@
+import { baseUrl } from '@/config';
+import { Organization, WithContext } from 'schema-dts';
+
+export default function ContactJsonLd() {
+  const jsonLd: WithContext<Organization> = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    url: baseUrl,
+    name: 'Scoutkåren Munksnäs Spejarna',
+    description: 'Finlandssvensk landscoutkår verksam i Munksnäs i västra Helsingfors',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Kadettvägen 2',
+      addressLocality: 'Helsingfors',
+      addressCountry: 'FI',
+      postalCode: '00330',
+    },
+  };
+
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />;
+}
